ebd242877c81779c7c66be1c17075fb5930a34a7
Anzai, Akihiro <mukojin.yoing@gmail.com>
2019-07-17 23:55:11 +0900

    Spring Boot Initializr

---
+++ src/test/java/net/rigibun/attendancemanager/AttendanceManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package net.rigibun.attendancemanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AttendanceManagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
